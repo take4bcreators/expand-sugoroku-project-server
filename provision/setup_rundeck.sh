d80a3243f30355bb36e827fd263928c6b67d0a81
hohyama <122266717+hiroki0720@users.noreply.github.com>
2023-03-30 21:03:48 +0900

    rundeckのSetupを追加 #8

--- provision/setup_rundeck.sh
+++ provision/setup_rundeck.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+# Rundeck セットアップシェル実行
+curl https://raw.githubusercontent.com/rundeck/packaging/main/scripts/rpm-setup.sh 2> /dev/null | bash -s rundeck 
 
-# ※ここに記述する
+# Rundeck・Javaインストール
+dnf -y install java-1.8.0 rundeck
+
+# Rundeck サービス起動
+systemctl start rundeckd
+
+# Rundeck 自動起動設定
+systemctl enable rundeckd
+
+# CLIインストール
+dnf -y install rundeck-cli
+
