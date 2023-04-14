fd59d8ff10b684afaba6eba6c6f887782e504973
hohyama <122266717+hiroki0720@users.noreply.github.com>
2023-04-14 21:08:39 +0900

    gatsbyの追加 #9

--- provision/setup_gatsby.sh
+++ provision/setup_gatsby.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-# ※ここに記述する
+# yumリポジトリの追加（現在は18が最新？）
+# 「https://github.com/nodejs/Release#nodejs-release-working-group」の「LTS」のバージョンを指定する
+curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+
+# Node.jsのインストール（★yumではなくdnfで行う）
+dnf install -y nodejs
+
+# yarn のインストール（★追加）
+npm install -g yarn
+
+# インストール（npmではなくyarnで行う）
+yarn global add gatsby-cli
+
+# Puppeteerインストール（HTML→PDF変換用のモジュール）
+yarn global add puppeteer
+
+
+exit 0
