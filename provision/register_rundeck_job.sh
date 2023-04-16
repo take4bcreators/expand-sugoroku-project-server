573248bf6990e9d82cbf40a04e41cf9da6614fe2
hdeg <76611318+exit1991@users.noreply.github.com>
2023-04-16 23:24:50 +0900

    Rundeck登録処理追加 #15 Co-authored-by: sono <asira-ship-it@users.noreply.github.com>

--- provision/register_rundeck_job.sh
+++ provision/register_rundeck_job.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+project_batch_root="/mnt/project/expand-sugoroku-project-batch"
+register_rundeck_dir="provision/register_rundeck"
 
 
+exec_shell="01_register_rundeck_job_unit.sh"
+if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
+    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
+else
+    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
+fi
 
+exec_shell="02_register_rundeck_job_group.sh"
+if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
+    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
+else
+    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
+fi
 
-
-
+exec_shell="03_register_rundeck_job_main.sh"
+if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
+    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
+else
+    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
+fi
 
 exit 0
