1f772de79320d0201e3dd1c423c6c2c230f86db7
hdeg <76611318+exit1991@users.noreply.github.com>
2023-04-21 19:37:26 +0900

    処理修正 #15

--- provision/register_rundeck_job.sh
+++ provision/register_rundeck_job.sh
@@ -2,27 +2,18 @@
 
 project_batch_root="/mnt/project/expand-sugoroku-project-batch"
 register_rundeck_dir="provision/register_rundeck"
+exec_shells="
+    01_register_rundeck_job_unit.sh
+    02_register_rundeck_job_group.sh
+    03_register_rundeck_job_main.sh
+"
 
-
-exec_shell="01_register_rundeck_job_unit.sh"
-if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
-    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
-else
-    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
-fi
-
-exec_shell="02_register_rundeck_job_group.sh"
-if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
-    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
-else
-    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
-fi
-
-exec_shell="03_register_rundeck_job_main.sh"
-if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
-    ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
-else
-    echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
-fi
+for exec_shell in ${exec_shells}; do
+    if [ -f "${project_batch_root}/${register_rundeck_dir}/${exec_shell}" ]; then
+        ${project_batch_root}/${register_rundeck_dir}/${exec_shell}
+    else
+        echo "${project_batch_root}/${register_rundeck_dir}/${exec_shell} が存在しません。スキップします"
+    fi
+done
 
 exit 0
